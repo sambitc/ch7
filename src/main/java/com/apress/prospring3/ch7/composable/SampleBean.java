c971657c39a9f79fcdee690fd6dcca6536249908
sambitchoudhury <sambitgudu@yahoo.co.in>
2014-11-12 19:56:36 +0530

    composble pointcut

---
+++ src/main/java/com/apress/prospring3/ch7/composable/SampleBean.java
@@ -0,0 +1,14 @@
+package com.apress.prospring3.ch7.composable;
+
+public class SampleBean {
+	public String getName() {
+		return "Clarence Ho";
+	}
+
+	public void setName(String name) {
+	}
+
+	public int getAge() {
+		return 100;
+	}
+}
